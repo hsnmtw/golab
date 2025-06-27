da6f07208a1c49bab33290ac6a7bfe0ec3de5a1d
hsnmtw <hussain.mutawa@gmail.com>
2025-06-27 17:33:39 +0300

    removed sqlite3 and used native go sqlite

--- lab3_sqliteDb/main.go
+++ lab3_sqliteDb/main.go
@@ -8,11 +8,12 @@ import (
 	"log"
 	"strings"
 
-	_ "github.com/mattn/go-sqlite3"
+	// _ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 func Direct() {
-	db, err := sql.Open("sqlite3", "./test.db")
+	db, err := sql.Open("sqlite", "./test.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,7 +34,7 @@ func Direct() {
 }
 
 type User struct {
-	Id int
+	Id   int
 	Name string
 }
 
@@ -42,26 +43,24 @@ func ORM() {
 	fmt.Println("connection state before open is : ", cn.GetState())
 	defer cn.Close()
 
-	if e := cn.Open("sqlite3", "./test.db"); e != nil {
+	if e := cn.Open("sqlite", "./test.db"); e != nil {
 		log.Fatal(e)
 	}
 
 	fmt.Println("connection state after open is : ", cn.GetState())
-	
-		_,e := cn.Exec(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        name TEXT
-    );
-    `)
 
-	if e!=nil {
-		log.Fatal("error while creating : ",e)
+	_, e := cn.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			name TEXT
+		);`)
+
+	if e != nil {
+		log.Fatal("error while creating : ", e)
 	}
-	
-	
-	affected, e := cn.Exec("INSERT INTO [users] ([name]) VALUES (@0)", "Hussain")
-	
+
+	affected, e := cn.Exec("INSERT INTO [users] ([name]) VALUES ($name)", sql.Named("name","test"))
+
 	if e != nil {
 		log.Fatal("error when inserting: ", e)
 	}
@@ -72,13 +71,13 @@ func ORM() {
 		log.Fatal("error when selecting: ", e)
 	}
 	b, _ := json.Marshal(data)
-	fmt.Println(strings.Join( strings.Split(string(b),"},{") ,"}\n,{"))
+	fmt.Println(strings.Join(strings.Split(string(b), "},{"), "}\n,{"))
 	fmt.Println("Connection State before closing is : ", cn.GetState())
 
 	var users []User
-	_ = cn.QueryUnmarshalled(&users,"select [Id],[Name] from users")
+	_ = cn.QueryUnmarshalled(&users, "select [Id],[Name] from users")
 	fmt.Println("Query unmarshalled")
-	for i,user := range users {
+	for i, user := range users {
 		fmt.Printf("\n [%d] User : id=%d, name=%s", i+1, user.Id, user.Name)
 	}
 
